a5faad0a3846401517390e8ebde4f91ac807454a
ashish8796 <ashishsaini.ak@gmail.com>
2021-01-26 23:34:10 +0530

    Deleted some Folders

---
+++ week-3/day-6/index.js
@@ -0,0 +1,4 @@
+import { printName } from "./stockItUp";
+// console.log(myName);
+var val = printName();
+console.log(val);
---
+++ week-3/day-6/stockItUp.js
@@ -0,0 +1,35 @@
+// You are given the distance travelled by car(in kilometres) in a certain time (in hours). If the speed of that car is greater than 40 km/hr, you have to print "Apply Brake" (without quotes), else in all other conditions, print "Keep Going" (without quotes)
+
+// module.export = {
+//   ,
+// };
+function printName() {
+  return "ASHISH KUMAR SAINI";
+}
+
+export { printName };
+// function runProgram(input) {
+//   var input = input.trim().split(" ").map(Number);
+//   var speed = input[0] / input[1];
+
+//   console.log(speed > 40 ? "Apply Brake" : "Keep Going");
+// }
+
+// process.stdin.resume();
+// process.stdin.setEncoding("ascii");
+// let read = "";
+// process.stdin.on("data", function (input) {
+//   read += input;
+// });
+// process.stdin.on("end", function () {
+//   read = read.replace(/\n$/, "");
+//   runProgram(read);
+// });
+
+// process.on("SIGINT", function () {
+//   read = read.replace(/\n$/, "");
+//   runProgram(read);
+//   process.exit(0);
+// });
+
+// split(/[\r\n]+/)
