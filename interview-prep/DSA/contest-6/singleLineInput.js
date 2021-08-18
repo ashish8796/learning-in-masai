d94dac043c0a18a9214193b14a22d64cd6ca547d
ashish8796 <ashishsaini.ak@gmail.com>
2021-08-18 23:12:35 +0530

    contest-6 is added

---
+++ interview-prep/DSA/contest-6/singleLineInput.js
@@ -0,0 +1,37 @@
+function handleTask(n, arr) {
+  console.log({ n, arr });
+}
+
+function runProgram(input) {
+  input = input.trim().split("\n");
+  const tCase = Number(input[0]);
+
+  for (let i = 0, line = 1; i < tCase; i++) {
+    const n = Number(input[line++]);
+    const arr = input[line++].trim().split(" ").map(Number);
+
+    handleTask(n, arr);
+  }
+}
+
+if (process.env.USERNAME === "ashish") {
+  runProgram(`10 7
+  0 2 4 4 5 5 7 7 9 10`);
+} else {
+  process.stdin.resume();
+  process.stdin.setEncoding("ascii");
+  let read = "";
+  process.stdin.on("data", function (input) {
+    read += input;
+  });
+  process.stdin.on("end", function () {
+    read = read.replace(/\n$/, "");
+    runProgram(read);
+  });
+
+  process.on("SIGINT", function () {
+    read = read.replace(/\n$/, "");
+    runProgram(read);
+    process.exit(0);
+  });
+}
